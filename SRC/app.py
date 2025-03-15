cf8a695f8cec23d8095913954c03d80bd7e0b599
murtathaalnajjar <murtadhanaj@gmail.com>
2025-03-15 16:14:39 +0300

    setup routs updated deign and added tasks

--- SRC/app.py
+++ SRC/app.py
@@ -9,17 +9,62 @@
 # POST prescription record to database
 ######################################
 
-
-
-
-
-
-
+#
+#
 # boilerplate start
 from flask import Flask, jsonify, request
 import sqlite3; from pages import *
 app = Flask(__name__)
 # boilerplate end
+#
+#
+
+
+
+
+
+
+
+# main page
+@app.route('/')
+def main():
+    return """ \
+        <a href="/field_clinic" target="_blank"><button>Field clinic / Pop-upclinic</button></a>
+        <a href="hospital" target="_blank"><button>Hospital</button></a>
+     """
+
+
+
+
+
+
+
+# field clinc page
+@app.route('/field_clinic')
+def field_clinic():
+    return """<h1>LIST</h1>"""
+
+
+
+
+
+
+
+
+# hospital page
+@app.route('/hospital')
+def hospital():
+    return """<h1>LIST</h1>"""
+
+
+
+
+
+
+
+@app.route('/list')
+def list(): # TODO: finish this
+    return jsonify({"data": "data"})
 
 
 
